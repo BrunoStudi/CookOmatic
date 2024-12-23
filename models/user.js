3430d384198867c42075a9dc6b6e5640b329d346
BrunoStudi <r.brunocarriere@protonmail.com>
2024-12-23 21:40:36 +0100

    modification inscription avec passport et session express + mongoose local startegy

--- models/user.js
+++ models/user.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const userSchema = new mongoose.Schema({
     pseudo: String,
     username: String,
     userlastname: String,
-    //userheight: Number,
-    //userweight: Number,
-    //userborn: Date,
+    userheight: Number,
+    userweight: Number,
+    userborn: Date,
     password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -2,7 +2,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
+const ejs = require("ejs");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,35 +19,94 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// initialisation de la session
+app.use(session({
+    secret: "mySecret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// initialisation du passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // connexion a mongoDB
 mongoose.connect('mongodb+srv://testDb:Asmae1985@cluster0.wy6vp.mongodb.net/omaticdb?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true }); //mongodb://0.0.0.0:27017/cookingDB
 
 // Models
 const User = require("./models/user");
 
-// Action des routes
-app.get("/signup", function (req, res) {
-    const userLoggedIn = false;
-    res.render("signup", { userLoggedIn });
+// passport mongoose
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Variables
+//let userLoggedIn = false;
+
+// Action des routes GET
+app.get("/login", function (req, res) {
+    res.render("login");
 });
 
-app.post("/signup", function (req,res) {
-    const user = new User ({
-        pseudo: req.body.pseudo,
-        username: req.body.name,
-        userlastname: req.body.lastname,
-        password: req.body.pwd
-    });
-    user.save()
-    .then(() => res.render("accueil"))
-    .catch((error) => res.status(500).json({ error }));
+app.get("/signup", function (req, res) {
+    res.render("signup");
 });
 
 app.get("/accueil", function (req, res) {
-    const userLoggedIn = false;
-    res.render("accueil", { userLoggedIn });
+    res.render("accueil");
 });
 
+// Action des routes POST
+app.post("/login", async function (req, res) {
+
+    try {
+        const foundUser = await User.findOne({ pseudo: req.body.pseudo });
+
+        if (foundUser) {
+            bcrypt.compare(req.body.pwd, foundUser.password, function (err, result) {
+                if (result) {
+                    userLoggedIn = true;
+                    res.render("accueil");
+                } else {
+                    res.status(401).send("Mot de passe incorrect");
+                }
+            });
+        } else {
+            res.status(404).send("Utilisateur non trouvé");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Erreur serveur");
+    }
+});
+
+app.post("/signup", function (req, res) {
+
+    const newUser = new User({
+        pseudo: req.body.pseudo,
+        username: req.body.name,
+        userlastname: req.body.lastname
+    });
+    User.register(newUser, req.body.pwd, function (err, user) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Erreur serveur");
+        }
+        if (!user) {
+            return res.status(400).send("Authentification échouée");
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Erreur serveur");
+            }
+            res.redirect("accueil");
+        });
+    });
+});
+
+// Action IMC
 app.post("/calcul-imc", function (req, res) {
     // Récupérer le poids et la taille
     const poids = parseFloat(req.body.poids);
